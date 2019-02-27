6519382d94ac1991e3438fc0c63f5ecfdf106978
Alexander Munz <a_munz@list.ru>
2019-02-27 12:51:13 +0300

    Добавляет поле добавления новых пунктов в todo-list

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,17 +3,42 @@ import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
 
+  state = {
+    label: ''
+  };
+
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onAdded(this.state.label);
+    this.setState({
+      label: ''
+    });
+  };
+
   render() {
     const { onAdded } =this.props;
 
     return (
-      <div className='item-add-form'>
+      <form
+        className='item-add-form d-flex'
+        onSubmit={ this.onSubmit }>
+        <input
+          type='text'
+          className='form-control'
+          onChange={ this.onLabelChange }
+          placeholder='What needs to be done?'
+          value={ this.state.label } />
         <button
-          className='btn btn-outline-primary'
-          onClick={ () => onAdded('Do something useful') }>
-          Add new item
+          className='btn btn-outline-primary'>
+          Add item
         </button>
-      </div>
+      </form>
     );
   }
 };
